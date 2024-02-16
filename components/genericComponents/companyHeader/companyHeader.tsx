ad9a3f59ef8730943757b646f1619f4b73d53b1b
Rafin Akther Utsahw <rafin.utshaw@gmail.com>
2024-02-17 00:35:02 +0200

    update cover

--- components/genericComponents/companyHeader/companyHeader.tsx
+++ components/genericComponents/companyHeader/companyHeader.tsx
@@ -1,11 +1,18 @@
 import Image from 'next/image'
+import { useParams } from 'next/navigation'
 import React from 'react'
 
 const CompanyHeader = () => {
+
+    const params = useParams()
+
     return (
         <div className='relative mb-4'>
-            <Image src={'/images/bg-black.png'} alt='coverImage' height={100} width={100} className='w-full h-20 border' />
-            <div className='absolute left-4 -bottom-4 shadow bg-white rounded-sm'><Image src={'/images/google-logo.png'} alt='company logo' className='w-14 h-14' height={100} width={100} /></div>
+            <Image src={'/images/company-cover.jpg'} alt='coverImage' height={100} width={100} className='w-full h-24 border' />
+            <div className='absolute left-4 bottom-4 rounded-sm flex items-center'>
+                <Image src={'/images/google-logo.png'} alt='company logo' className='w-14 h-14 shadow bg-white' height={100} width={100} />
+                <div className='ms-2 font-bold uppercase'>{params?.companyHandle}</div>
+            </div>
         </div>
     )
 }
